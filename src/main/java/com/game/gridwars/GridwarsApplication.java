fd468966af5e2624ef5652facd12558fee5284a8
Jose Paternina <jose.paternina.a@gmail.com>
2021-08-27 18:49:26 -0500

    Initial commit

---
+++ src/main/java/com/game/gridwars/GridwarsApplication.java
@@ -0,0 +1,13 @@
+package com.game.gridwars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GridwarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GridwarsApplication.class, args);
+	}
+
+}
